024b2f4a0c2aa31898fe024851d84abc25b3dd6b
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-16 18:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //your code here
+function sortBandNames(names){
+	return names.sort((a,b) =>{
+		const removeArticles=(name) =>(name).replace(/^(a |an |the )/i, '');
+		const nameA=removeArticles(a.toLowerCase());
+		const nameB=removeArticles(b.toLowerCase());
+		return nameA.localeCompare(nameB);
+	});
+function displayBandNames(names) {
+  const ul = document.getElementById('band');
+  ul.innerHTML = '';
 
-let touristSpots = ['The Virupaksha Temple', 'an Victoria Memorial', 'a xTajmahal'];
+  // Sort the band names
+  const sortedNames = sortBandNames(names);
 
-function tri(name){
-	 return name.replace(/^(a |the |an )/i,'');
-
- }
-
- let a= touristSpots.sort((a,b) => tri(a)>tri(b) ? 1 : -1 );
- console.log(a);
+  // Create and append list items to the HTML list
+  sortedNames.forEach((name) => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+  });
+	
+}
